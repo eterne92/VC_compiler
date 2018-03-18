58e906e488fdcc4dfd65ec3fa44b4bc881a44c8c
eterne92 <shaohui920308@gmail.com>
2018-03-18 20:18:57 +1100

    nothing

--- vc.java
+++ vc.java
@@ -1,27 +1,33 @@
-/*+*
+/*
+ * vc.java           
  *
- * vc.java           67/2/2017
+ * F O R     A S S I G N M E N T    2
+ * 
  * 
  * Jingling Xue, CSE, UNSW, Sydney NSW 2052, Australia.
- *
- *+*/
+ * 
+ */
 
 package VC;
 
 import VC.Scanner.Scanner;
 import VC.Scanner.SourceFile;
-import VC.Scanner.Token;
+import VC.Recogniser.Recogniser;
 
 public class vc {
 
     private static Scanner scanner;
     private static ErrorReporter reporter;
-    private static Token currentToken;
+    private static Recogniser recogniser;
+
     private static String inputFilename; 
 
- 
     public static void main(String[] args) {
-        inputFilename = args[0];
+        if (args.length != 1) {
+          System.out.println("Usage: java VC.vc filename\n");
+          System.exit(1); 
+        } else
+           inputFilename = args[0];
 
         System.out.println("======= The VC compiler =======");
 
@@ -29,10 +35,14 @@ public class vc {
 
         reporter = new ErrorReporter();
         scanner  = new Scanner(source, reporter);
-        scanner.enableDebugging();
+        recogniser = new Recogniser(scanner, reporter);
 
-        do 
-	  currentToken = scanner.getToken();
-        while (currentToken.kind != Token.EOF);
+        recogniser.parseProgram();
+
+        if (reporter.numErrors == 0)
+           System.out.println ("Compilation was successful.");
+        else
+           System.out.println ("Compilation was unsuccessful.");
     }
 }
+
